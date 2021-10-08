72f2d971e83fc7124b116365b91feecce38b360b
thiago-scherrer <eu@thiago.info>
2021-10-08 07:30:40 -0300

    add mock files

--- src/pyholebot/core.py
+++ src/pyholebot/core.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+import get_list
 
 
+def main():
+    pyhole = get_list.UrlList()
+    print(pyhole.env_setup())
+    pyhole.cleanup()
+    pyhole.get_endpoints()
+    pyhole.url_uniq()
 
 
-
+if __name__ == "__main__":
+    main()
--- src/pyholebot/get_list.py
+++ src/pyholebot/get_list.py
@@ -1,28 +1,95 @@
-#!/usr/bin/env python
 import os
+import re
 import urllib.request
 
 
 class UrlList:
-    def get_endpoints(self):
+
+    def __init__(self):
+        self.tmpfile = '/tmp/blocklist.txt'
+        self.endpoint = None
+        self.output = None
+
+    def env_setup(self):
+        try:
+            self.endpoint = os.environ['URL_FILE']
+        except KeyError:
+            return "ENV URL_FILE required!"
 
         try:
-            endpoints = os.environ['URL_FILE']
+            self.output = os.environ['BLOCKLIST_OUTPUT']
+        except KeyError:
+            return "ENV BLOCKLIST_OUTPUT required!"
+
+    def get_endpoints(self):
+        self.env_setup()
+
+        try:
             result = []
-            with open(endpoints) as f:
+            with open(self.endpoint) as f:
                 lines = f.readlines()
-                for endpoint in lines:
-                    self.get_block_list(endpoint)
-                    result.append(endpoint)
+                for self.endpoint in lines:
+                    self.get_block_list(self.endpoint)
+                    result.append(self.endpoint)
                 return result
 
         except FileNotFoundError:
             return "Endpoint file not found!"
 
-        except KeyError:
-            return "Env URL_FILE required!"
-
     def get_block_list(self, endpoint):
+        try:
+            with urllib.request.urlopen(endpoint) as f:
+                result = f.read().decode('utf-8')
+                normalized = self.url_normalizer(result)
+                self.create_blocklist(normalized)
+                return result
 
-        with urllib.request.urlopen(endpoint) as f:
-            return(f.read().decode('utf-8'))
+        except Exception:
+            print(f"Failed trying to access endpoint: {endpoint}")
+            pass
+
+    def url_normalizer(self, blocklist):
+        regex = re.compile(
+                r'(?m)^#(.*)|'
+                r'(?m)^repo(.*)|'
+                r'(0.0.0.0)|'
+                r'(127.0.0.1)|'
+                r'(localhost)|'
+                r'((?m)@)|'
+                r'((?m)\:(.*))|'
+                r'(<br>)|'
+                r'(<BR>)|'
+                r'(\s\s)|'
+                r'( )|', re.IGNORECASE)
+        return re.sub(regex, '', blocklist)
+
+    def create_blocklist(self, blocklist):
+        tmp = self.tmpfile
+        fo = open(tmp, 'a')
+        return fo.write(blocklist)
+
+    def url_uniq(self):
+        self.env_setup()
+
+        lines_seen = set()
+
+        if os.path.exists(self.output):
+            os.remove(self.output)
+
+        try:
+            output_blocklist = open(self.output, 'x')
+            for line in open(self.tmpfile, "r"):
+                if line not in lines_seen:
+                    output_blocklist.write(line)
+                    lines_seen.add(line)
+            output_blocklist.close()
+
+        except KeyError:
+            return "ENV BLOCKLIST_OUTPUT required!"
+
+    def cleanup(self):
+        if os.path.exists(self.tmpfile):
+            os.remove(self.tmpfile)
+
+        f = open(self.tmpfile, "x")
+        return f.closed
